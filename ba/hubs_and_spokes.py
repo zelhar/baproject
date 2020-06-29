085c2ebaa874c6b6cc802234a4e4d003485989f7
Yiftach Kolb <zelhar@gmail.com>
2020-06-29 18:33:09 +0200

    df

--- ba/hubs_and_spokes.py
+++ ba/hubs_and_spokes.py
@@ -138,6 +138,7 @@ def biasedPropagate(A, bias, alpha=0.85, beta=1, epsilon=1e-7, maxiter=10 ** 7):
             x = y
     return x.flatten(), W, t
 
+
 def biasedPropagateG(G, bias, alpha=0.85, beta=1, epsilon=1e-7, maxiter=10 ** 7):
     """The inputs: G: a networkx type graph.
     bias: The biased restart distribution. 
@@ -240,6 +241,27 @@ def extendLabel(G, label=[0], alpha=0.85):
     return ext_label2
 
 
+def pageRanksConcentratedBiasG(G, alpha=0.85):
+    """Input: graph G, restart parameter alpha, and stopping criterions.
+    output: Graph G with additional properties. For each vertex v, it adds 
+    property 'br_v' so that br_v[i] is the pagerank of node i when we use
+    restart distribution that is concentrated on v.
+    The nodes are assumed to be represented by integers.
+    In addition the same data is returned as ad-array, where the stationary
+    distribution with bias on node i is the ith raw.
+    """
+    n = len(G.nodes())
+    W = np.zeros((n, n))
+    for vertex in tqdm(G.nodes()):
+        bias = np.zeros(n)
+        bias[vertex] = 1
+        p, _ = biasedPropagateG(G, bias=bias, alpha=alpha)
+        W[vertex] = p
+        for i in G.nodes():
+            G.nodes[i]["br_" + str(vertex)] = p[i]
+    return G, W
+
+
 ################## Graph experiments ######################################
 # Maybe the simplest connected graph is a star, and mor generally - a hub and
 # spoke grpaph. So every node is either a leaf or a hub. The problem is- every
@@ -281,7 +303,7 @@ plt.bar(range(len(p)), p)
 
 # add a node, repeat the experiment.
 G.add_node(3)
-G.add_edge(0,3)
+G.add_edge(0, 3)
 nx.draw(G, with_labels=True)
 
 p, W = powerIterateG(G, alpha=0.99)
@@ -301,7 +323,7 @@ p[:2]
 plt.bar(range(len(p)), p)
 
 G.add_node(4)
-G.add_edge(0,4)
+G.add_edge(0, 4)
 nx.draw(G, with_labels=True)
 
 p, W = powerIterateG(G, alpha=0.99)
@@ -321,7 +343,7 @@ p[:2]
 plt.bar(range(len(p)), p)
 
 G.add_node(5)
-G.add_edge(0,5)
+G.add_edge(0, 5)
 nx.draw(G, with_labels=True)
 
 p, W = powerIterateG(G, alpha=0.99)
@@ -343,9 +365,9 @@ plt.bar(range(len(p)), p)
 # So we see that with alpha close to 1, the hub's pagerank approaches .5
 # Now what happens when we connect two hubs?
 
-G.add_edges_from([range(6,20)])
-G.add_edges_from([(6,i) for i in range(7,20)])
-G.add_edge(0,6)
+G.add_edges_from([range(6, 20)])
+G.add_edges_from([(6, i) for i in range(7, 20)])
+G.add_edge(0, 6)
 nx.draw(G, with_labels=True)
 
 # The bigger star wins
@@ -367,36 +389,15 @@ p[:]
 plt.bar(range(len(p)), p)
 
 
-def pageRanksConcentratedBiasG(G, alpha=0.85, epsilon=1e-7, maxiter=10**7):
-    """Input: graph G, restart parameter alpha, and stopping criterions.
-    output: Graph G with additional properties. For each vertex v, it adds 
-    property 'br_v' so that br_v[i] is the pagerank of node i when we use
-    restart distribution that is concentrated on v.
-    The nodes are assumed to be represented by integers.
-    """
-    n = len(G.nodes())
-    for vertex in tqdm(G.nodes()):
-        bias = np.zeros(n)
-        bias[vertex] = 1
-        p,_ = biasedPropagateG(G, bias, alpha, epsilon, maxiter)
-        for i in G.nodes():
-            G.nodes[i]["br_" + str(vertex)] = p[i]
-    return G
-
 H = pageRanksConcentratedBiasG(G)
 
 
-p, _ = biasedPropagateG(G, bias=[1,0,0,0])
-
-
-
+p, _ = biasedPropagateG(G, bias=[1, 0, 0, 0])
 
 
 ### Trying to import graphml network
 
-G = nx.readwrite.read_graphml(
-        'IMExEColi.graphml',
-        node_type=int)
+G = nx.readwrite.read_graphml("IMExEColi.graphml", node_type=int)
 
 nx.draw(G)
 
@@ -404,7 +405,7 @@ plt.show()
 
 G.nodes()
 
-G.nodes['537']
+G.nodes["537"]
 
 G.edges()
 
@@ -413,7 +414,112 @@ G.edges[0]
 
 p, W = powerIterateG(G, alpha=0.85, directmethod=True)
 
-p = biasedPropagateG(G, bias=[1,0,0,0] ,alpha=0.85)
+p = biasedPropagateG(G, bias=[1, 0, 0, 0], alpha=0.85)
 
 p[0:10]
 plt.bar(range(len(p)), p)
+
+
+# Testing a little spider graph
+G = nx.Graph()
+G.add_nodes_from(range(20))
+G.add_edges_from([(0, i) for i in range(1, 8)])
+
+G.add_edge(0, 8)
+G.add_edge(8, 9)
+G.add_edges_from([(9, i) for i in range(10, 15)])
+
+G.add_edge(15, 9)
+
+G.add_edges_from([(i, j) for i in range(15, 19) for j in range(i + 1, 20)])
+
+nx.draw(G, with_labels=True)
+plt.show()
+
+plt.close()
+
+H, W = pageRanksConcentratedBiasG(G, alpha=0.85)
+
+plt.figure(figsize=(8, 8))
+nx.draw(H, with_labels=True, node_color=W[8], node_size=W[8]*10000)
+plt.title("the little spider graph.")
+plt.show()
+
+plt.colorbar(W[0])
+
+plt.pcolormesh(W)
+
+plt.imshow(W, cmap='hot', interpolation='nearest')
+
+plt.colorbar()
+
+fig, ax = plt.subplots()
+im = ax.imshow(W, cmap='hot', interpolation='nearest')
+cbar = ax.figure.colorbar(im)
+ax.set_xticks(np.arange(20))
+ax.set_yticks(np.arange(20))
+ax.set_title("Heatmap of the biased page ranks for the little spider graph.")
+
+def heatmap(mat, title):
+    """Help function designed to plot a heatmap of
+    the biased stationaries array of a graph.
+    """
+    n,m = mat.shape
+    fig, ax = plt.subplots()
+    im = ax.imshow(mat, cmap='hot', interpolation='nearest')
+    cbar = ax.figure.colorbar(im)
+    ax.set_xticks(np.arange(m))
+    ax.set_yticks(np.arange(n))
+    ax.set_title(title)
+
+plt.savefig("Heatmap Little Spider.png")
+
+H, W50 = pageRanksConcentratedBiasG(G, alpha=0.5)
+heatmap(W50, "Heatmap corresponding to alpha=0.5")
+plt.savefig("Heatmap Little Spider alpha=0.5.png")
+plt.close()
+
+
+H, W10 = pageRanksConcentratedBiasG(G, alpha=0.1)
+heatmap(W10, "Heatmap corresponding to alpha=0.1")
+plt.savefig("Heatmap Little Spider alpha=0.1.png")
+plt.close()
+
+H, W90 = pageRanksConcentratedBiasG(G, alpha=0.9)
+heatmap(W90, "Heatmap corresponding to alpha=0.9")
+plt.savefig("Heatmap Little Spider alpha=0.9.png")
+plt.close()
+
+H, W25 = pageRanksConcentratedBiasG(G, alpha=0.25)
+heatmap(W25, "Heatmap corresponding to alpha=0.25")
+plt.savefig("Heatmap Little Spider alpha=0.25.png")
+plt.close()
+
+H, W85 = pageRanksConcentratedBiasG(G, alpha=0.85)
+heatmap(W85, "Heatmap corresponding to alpha=0.85")
+plt.savefig("Heatmap Little Spider alpha=0.85.png")
+plt.close()
+
+b = np.zeros(len(W))
+b[16]=1
+
+nx.draw(H, with_labels=True, node_color=W[8], node_size=W[8]*10000)
+
+p, _ = biasedPropagateG(G, bias=b, alpha=0.85)
+
+p[15:20]
+
+plt.bar(range(20), p)
+plt.bar(range(len(p)), p)
+
+y = np.zeros((20,20))
+for i in range(20):
+    b = np.zeros(20)
+    b[i]=1
+    y[i], _ = biasedPropagateG(G, b, 0.85)
+
+#y[16] = p
+
+heatmap(p.reshape((1,20)), "")
+
+heatmap(y, "")
